861d7c64f7be6c6ab5f3831cf16c8742bb3d8e4e
Juan Miguel Cejuela <i@juanmi.rocks>
2017-06-16 16:32:50 +0200

    add my content

--- src/App.js
+++ src/App.js
@@ -10,21 +10,43 @@ class App extends Component {
           <header>
             <span className="avatar"><img src="https://twitter.com/juanmirocks/profile_image?size=original" alt="" /></span>
             <h1>Sup, I'm Juanmi</h1>
-            <p>Senior Psychonautics Engineer</p>
+
+            <p>I'm a data scientist by profession, all text mining things, <a href="https://en.wikipedia.org/wiki/Natural_language_processing">#nlp</a>, <a href="https://en.wikipedia.org/wiki/Natural_language_understanding">#nlu</a>. I'm currently working on <a href="https://www.tagtog.net">tagtog</a>. I'm also happy to help you with 📧 <a href="mailto:consult@juanmi.rocks">short-term consultant projects</a>. Let's connect:</p>
+
+            <ul className="icons">
+              <li><a href="https://github.com/juanmirocks" className="fa-github">GitHub</a></li>
+              <li><a href="https://stackoverflow.com/users/341320/juanmirocks" className="fa-stack-overflow">StackOverflow</a></li>
+              <li><a href="https://www.linkedin.com/in/juanmirocks" className="fa-linkedin">LinkedIn</a></li>
+              <li><a href="mailto:consult@juanmi.rocks" className="fa-envelope-o">E-Mail</a></li>
+              {/* ResearchGate
+              Academia
+              Angel.co
+              Medium
+              Tumblr
+              Wordpress
+              About.me */}
+            </ul>
+
+            <br/>
+
+            <p>I live my own way. My passion is in connecting people and ideas, to foster peace, collaboration, and friendship. Here you find the unfiltered side of me:</p>
+            <ul className="icons">
+              <li><a href="https://www.instagram.com/juanmirocks" className="fa-instagram">Instagram</a></li>
+              <li><a href="https://twitter.com/juanmirocks" className="fa-twitter">Twitter</a></li>
+              {/* <li><a href="https://www.youtube.com/channel/UCXEObTEkoKT2kRxl5FLx86Q" className="fa-youtube">YouTube</a></li> */}
+              <li><a href="https://www.goodreads.com/juanmirocks" className="fa-book">Goodreads</a></li>
+              {/* Facebook
+              Snapchat
+              Musically */}
+            </ul>
+
           </header>
 
-          <footer>
-            <ul className="icons">
-              <li><a href="#s" className="fa-twitter">Twitter</a></li>
-              <li><a href="#b" className="fa-instagram">Instagram</a></li>
-              <li><a href="#c" className="fa-facebook">Facebook</a></li>
-            </ul>
-          </footer>
         </section>
 
         <footer id="footer">
           <ul className="copyright">
-            <li>&copy; Juan Miguel Cejuela</li>
+            <li>&copy; Juan Miguel Cejuela, aka, <em>@juanmirocks</em></li>
           </ul>
         </footer>
 
